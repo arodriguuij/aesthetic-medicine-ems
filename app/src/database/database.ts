78dd5573d735a99aa5a6932fc35f4ab992657406
arodriguuij <arodriguuij@alumnos.unex.es>
2024-08-06 10:20:18 +0200

    feat: differentiate between local and prod DB

--- app/src/database/database.ts
+++ app/src/database/database.ts
@@ -13,14 +13,27 @@ export const _getDataBaseConnection = () => {
 
   console.log("SSL Config:", sslConfig); // Agregar mensaje de depuración
 
-  const poolConfig: PoolConfig = {
-    host: process.env.NEXT_PUBLIC_HOST_DATA_BASE,
-    port: +(process.env.NEXT_PUBLIC_PORT_DATA_BASE || POSTGRESQL_DEFAULT_PORT),
-    user: process.env.NEXT_PUBLIC_USER_DATA_BASE,
-    password: process.env.NEXT_PUBLIC_PASSWORD_DATA_BASE,
-    database: process.env.NEXT_PUBLIC_NAME_DATA_BASE,
-    ...sslConfig,
-  };
+  const poolConfig: PoolConfig = process.env.NEXT_PUBLIC_LOCAL_DATA_BASE
+    ? {
+        host: process.env.NEXT_PUBLIC_HOST_DATA_BASE,
+        port: +(
+          process.env.NEXT_PUBLIC_PORT_DATA_BASE || POSTGRESQL_DEFAULT_PORT
+        ),
+        user: process.env.NEXT_PUBLIC_USER_DATA_BASE,
+        password: process.env.NEXT_PUBLIC_PASSWORD_DATA_BASE,
+        database: process.env.NEXT_PUBLIC_NAME_DATA_BASE,
+        ...sslConfig,
+      }
+    : {
+        host: process.env.NEXT_PUBLIC_PROD_HOST_DATA_BASE,
+        port: +(
+          process.env.NEXT_PUBLIC_PROD_PORT_DATA_BASE || POSTGRESQL_DEFAULT_PORT
+        ),
+        user: process.env.NEXT_PUBLIC_PROD_USER_DATA_BASE,
+        password: process.env.NEXT_PUBLIC_PROD_PASSWORD_DATA_BASE,
+        database: process.env.NEXT_PUBLIC_PROD_NAME_DATA_BASE,
+        ...sslConfig,
+      };
   console.log("Database Connection Config:", poolConfig); // Agregar mensaje de depuración
 
   return new Pool(poolConfig);
