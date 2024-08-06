e1872cf6fddfde46d83aad8ec2a51028f5fec8a7
arodriguuij <arodriguuij@alumnos.unex.es>
2024-08-06 11:02:49 +0200

    feat: refactor dabase.ts

--- app/src/database/database.ts
+++ app/src/database/database.ts
@@ -3,28 +3,16 @@ import _once from "lodash/once";
 
 export const POSTGRESQL_DEFAULT_PORT = 5432;
 export const _getDataBaseConnection = () => {
-  const sslConfig = process.env.NEXT_PUBLIC_LOCAL_DATA_BASE
-    ? {}
-    : {
-        ssl: {
-          rejectUnauthorized: false,
-        },
-      };
-
-  console.log("SSL Config:", sslConfig, process.env.NEXT_PUBLIC_LOCAL_DATA_BASE); // Agregar mensaje de depuración
-
-  const poolConfig: PoolConfig = process.env.NEXT_PUBLIC_LOCAL_DATA_BASE
-    ? {
-        host: process.env.NEXT_PUBLIC_HOST_DATA_BASE,
-        port: +(
-          process.env.NEXT_PUBLIC_PORT_DATA_BASE || POSTGRESQL_DEFAULT_PORT
-        ),
-        user: process.env.NEXT_PUBLIC_USER_DATA_BASE,
-        password: process.env.NEXT_PUBLIC_PASSWORD_DATA_BASE,
-        database: process.env.NEXT_PUBLIC_NAME_DATA_BASE,
-        ...sslConfig,
-      }
-    : {
+  let poolConfig: PoolConfig;
+  console.log({
+    env: process.env.NEXT_PUBLIC_DATA_BASE_ENVIRONMENT,
+    checkProd: process.env.NEXT_PUBLIC_DATA_BASE_ENVIRONMENT === "prod",
+    checkLocalhost:
+      process.env.NEXT_PUBLIC_DATA_BASE_ENVIRONMENT === "localhost",
+  });
+  switch (process.env.NEXT_PUBLIC_DATA_BASE_ENVIRONMENT) {
+    case "prod":
+      poolConfig = {
         host: process.env.NEXT_PUBLIC_PROD_HOST_DATA_BASE,
         port: +(
           process.env.NEXT_PUBLIC_PROD_PORT_DATA_BASE || POSTGRESQL_DEFAULT_PORT
@@ -32,9 +20,29 @@ export const _getDataBaseConnection = () => {
         user: process.env.NEXT_PUBLIC_PROD_USER_DATA_BASE,
         password: process.env.NEXT_PUBLIC_PROD_PASSWORD_DATA_BASE,
         database: process.env.NEXT_PUBLIC_PROD_NAME_DATA_BASE,
-        ...sslConfig,
+        ssl: {
+          rejectUnauthorized: false,
+        },
       };
-  console.log("Database Connection Config:", poolConfig); // Agregar mensaje de depuración
+      break;
+
+    case "localhost":
+      poolConfig = {
+        host: process.env.NEXT_PUBLIC_HOST_DATA_BASE,
+        port: +(
+          process.env.NEXT_PUBLIC_PORT_DATA_BASE || POSTGRESQL_DEFAULT_PORT
+        ),
+        user: process.env.NEXT_PUBLIC_USER_DATA_BASE,
+        password: process.env.NEXT_PUBLIC_PASSWORD_DATA_BASE,
+        database: process.env.NEXT_PUBLIC_NAME_DATA_BASE,
+      };
+      break;
+    default:
+      poolConfig = {};
+      break;
+  }
+
+  console.log("Database Connection Config:", poolConfig);
 
   return new Pool(poolConfig);
 };
