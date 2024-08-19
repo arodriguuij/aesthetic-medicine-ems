1fe29f02ddceb51283c15ad92b676800256f8469
arodriguuij <arodriguuij@alumnos.unex.es>
2024-08-19 22:24:59 +0200

    fix: adapt padding

--- app/contact/ScheduleOfToday/ScheduleOfToday.tsx
+++ app/contact/ScheduleOfToday/ScheduleOfToday.tsx
@@ -4,7 +4,7 @@ import { getScheduleOfToday } from "../contact.utils";
 const ScheduleOfToday = () => {
   const schedule = getScheduleOfToday();
   return (
-    <div className="mt-4">
+    <div className="mx-auto sm:px-6 mt-4 lg:max-w-7xl lg:px-8">
       {schedule ? (
         <h2 className="text-base font-semibold leading-7 text-amber-500">
           Hoy, la clinica está abierta: {schedule}
