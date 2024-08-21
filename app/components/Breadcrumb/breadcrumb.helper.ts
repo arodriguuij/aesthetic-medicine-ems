89100ee228000ff00c8bc922711ddee35ed0717e
arodriguuij <arodriguuij@alumnos.unex.es>
2024-08-21 16:05:57 +0200

    feat: stlyes and colors

--- app/components/Breadcrumb/breadcrumb.helper.ts
+++ app/components/Breadcrumb/breadcrumb.helper.ts
@@ -12,13 +12,18 @@ export const getPages = (
     const isLast = index === splitString.length - 1;
 
     let readableTitle = part;
-    if (part === "treatments") readableTitle = "Tratamientos";
+    if (part === "treatments") {
+      if (splitString.length === 3) readableTitle = "Ttos.";
+      else readableTitle = "Tratamientos";
+    }
     if (part === "products") readableTitle = "Productos";
     if (part === "giftCard") readableTitle = "Tarjeta regalo";
     if (part === "aboutMe") readableTitle = "Sobre mi";
     if (part === "contact") readableTitle = "Contacto";
     if (part === "cart") readableTitle = "Carrito";
     if (part === "paymentSuccess") readableTitle = "Confirmacion pago";
+    if (part === "facial") readableTitle = "Faciales";
+    if (part === "corporal") readableTitle = "Corporales";
 
     if (typeof +part === "number" && +part > 0) {
       if (splitString[index - 1] === "products" && products)
