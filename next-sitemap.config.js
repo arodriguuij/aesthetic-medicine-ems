773b9ef24e3294fa63692f9899f5ada264719d4d
arodriguuij <arodriguuij@alumnos.unex.es>
2024-09-05 19:58:19 +0200

    feat: add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    siteUrl: 'https://www.medicinaesteticaems.com', // Reemplaza con la URL de tu sitio
+    generateRobotsTxt: true, // Opcional: genera también el archivo robots.txt
+  };
