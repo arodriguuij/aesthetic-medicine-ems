c7a77907bf63236c88ac8dcf123a268b279cfd91
arodriguuij <arodriguuij@alumnos.unex.es>
2024-11-04 12:55:38 +0100

    feat: add Maintanance mode

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,6 @@ import autoCert from "anchor-pki/auto-cert/integrations/next";
 const withAutoCert = autoCert({
   enabledEnv: "development",
 });
-console.log({env: process.env.NEXT_PUBLIC_MAINTENANCE_MODE})
 
 const nextConfig = {
   reactStrictMode: true,
