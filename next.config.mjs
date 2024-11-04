b4e136406273184977c53b8c637d406b42e1f84d
arodriguuij <arodriguuij@alumnos.unex.es>
2024-11-04 12:21:23 +0100

    feat: add Maintanance mode

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,21 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["res.cloudinary.com"],
-    remotePatterns: [{ protocol: "https", hostname: "res.cloudinary.com", port:'' }],
+    remotePatterns: [
+      { protocol: "https", hostname: "res.cloudinary.com", port: "" },
+    ],
+  },
+
+  redirects() {
+    return [
+      process.env.MAINTENANCE_MODE === "1"
+        ? {
+            source: "/((?!maintenance).*)",
+            destination: "/maintenance.html",
+            permanent: false,
+          }
+        : null,
+    ].filter(Boolean);
   },
 };
 
